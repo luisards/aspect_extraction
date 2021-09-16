b7eb48a70b117e697c591d43e17a3191acaca897
Luisa Ribeiro <luisa.ribeiro94@gmail.com>
2021-09-16 18:41:16 +0200

    extract XML data

---
+++ example_pipeline/extract_data.py
@@ -0,0 +1,22 @@
+from xml.etree import cElementTree as ET
+import os
+def extractXML(folder, out_dir):
+    files = os.listdir(folder)
+    count = 0
+    for filename in files:
+        if 'xml' in filename:
+            dir = os.path.join(folder, filename)
+            #file_name = os.path.basename(dir)
+            with open(dir, 'r') as f:
+                tree = ET.parse(f)
+                root = tree.getroot()
+                new_filename = filename.replace("xml", "txt")
+            with open(out_dir + new_filename, "w") as Outfile:
+                for page in root.iter('text'):
+                    line = str(page.text)
+                    Outfile.write(line + " \n")
+                Outfile.close()
+            f.close()
+
+if __name__ == "__main__":
+    extractXML("C:/Users/PC/anaconda3/envs/rnlp/cat/cat/data", 'C:/Users/PC/anaconda3/envs/rnlp/cat/cat/data/cleanData/')
